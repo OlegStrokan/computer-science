fac0a6fcc751c5c408dbd4ee65fa64d6fd56adf1
Oleh Strokan <oleh.strokan@enrian.com>
2022-09-09 16:21:30 +0200

    Reactive framework: Update patch function

--- reactive-framework/index.js
+++ reactive-framework/index.js
@@ -32,7 +32,6 @@ function h(tag, props, children) {
 
 
 // Mount a virtual node of the DOM
-
 function mount(vnode, container) {
     const el = document.createElement(vnode.tag)
 
@@ -66,21 +65,41 @@ function patch(n1, n2) {
     if (n1.tag !== n2.tag) {
         mount(n2, n1.el.parentNode)
         unmount(n1)
-    }
-
-    else {
-        if (typeof n1.children === 'string') {
-            n2.el = n1.el
+    } else {
+        n2.el = n1.el
+        if (typeof n2.children === 'string') {
             n2.el.textContent = n2.children
         } else {
-            while(n2.el.attributes.length > 0)
-            n2.el.removeAttribute(n2.el.attributesp[0].name)
+            while (n2.el.attributes.length > 0)
+                n2.el.removeAttribute(n2.el.attributesp[0].name)
 
             for (const key in n2.props) {
                 n2.el.setAttribute(key, n2.props[key])
             }
-        }
 
+            if (typeof n1.children === 'string') {
+                n2.el.textContent = null;
+                n2.el.forEach((child) => {
+                    mount(child, n2.el)
+                })
+            } else {
+                const commonLength = Math.min(n1.children.length, n2.chilren.length)
+
+                for (let i = 0; i < commonLength; i++) {
+                    patch(n1.children[i], n2.children[i])
+                }
+
+                if (n1.children.length > n2.chidlren.length) {
+                    n1.children.slice(n2.children.length).forEach((child) => {
+                        unmount(child)
+                    })
+                } else if (n2.children.length > n1.chidlren.length) {
+                    n2.children.slice(n1.children.length).forEach((child) => {
+                        mount(child)
+                    })
+                }
+            }
+        }
     }
 
 }
