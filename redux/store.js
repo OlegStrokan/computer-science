8cc558a678e80a5f901fc947e8ae21899ae3644b
Oleh Strokan <oleh.strokan@enrian.com>
2022-10-01 15:15:44 +0200

    Complete store

--- redux/store.js
+++ redux/store.js
@@ -10,14 +10,29 @@ const validateAction = action => {
 }
 
 const createStore = (reducer) => {
-    let state = undefined
-    return {
+    let state
+    const subscribers = []
+    const store = {
         dispatch: (action) => {
             validateAction(action)
             state = reducer(state,action)
+            subscribers.forEach(handler => handler())
         },
-        getState: () => state
+        getState: () => state,
+        subscribe: handler => {
+            subscribers.push(handler)
+            return () => {
+                const index = subscribers.indexOf(handler)
+                if (index > 0) {
+                    subscribers.splice(index, 1)
+                }
+            }
+        }
     }
+
+    store.dispatch({ type: "@@redux/INIT" })
+    return store
+
 }
 
 
