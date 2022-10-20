8b6507170a56da8527a0488ba48d0409f683b133
Oleh Strokan <oleh.strokan@enrian.com>
2022-10-20 22:47:33 +0200

    Memory allocator: Add malloc function

--- malloc/main.c
+++ malloc/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+ #include <stdint.h>
 #include <assert.h>
 
 
@@ -10,25 +10,60 @@
 #define HEAP_SIZE STACK_SIZE * 4
 #define HEADER 4
 
+static u16 IN_USE;
+
 typedef struct virtual_memory {
     u8 stack[STACK_SIZE];
-    char** unmapped;
+    char **unmapped;
     u8 heap[HEAP_SIZE];
 
-    struct  {
-        char** data;
-        char** bss;
-        char* test;
-    }data_t;
-}virtual_memory_t;
+    struct {
+        char **data;
+        char **bss;
+        char *test;
+    } data_t;
+} virtual_memory_t;
 
-void* my_malloc(size_t size) {
+typedef struct entity {
+    u8 *ptr;
+    u16 size;
+} entity_t;
+
+entity_t LIST[40];
+
+entity_t* new_entity(size_t size) {
+    if (LIST[0].ptr == NULL && LIST[0].size == 0) {
+        static virtual_memory_t vm;
+        LIST[0].ptr = vm.heap;
+        LIST[0].size = HEAP_SIZE;
+        IN_USE++;
+    }
+}
+
+void *my_malloc(size_t size) {
+    // if size less than max heap size
     assert(size <= HEAP_SIZE);
+    // header fro meta data
+    size += HEADER;
+
+    entity_t *e = new_entity(size);
+
+    u8* start = e->ptr;
+    u8* user_ptr = start + HEADER;
+
+    *start = size;
+    e->ptr += size;
+    e->size -= size;
+
+    return user_ptr;
+
+
 }
 
-void my_free(void* pt) {
-    int* a = my_malloc(sizeof(int));
+void my_free(void *pt) {
+    int *a = my_malloc(sizeof(int));
 }
+
 void test() {
 
 }
