84681a7917b89b584a42a1cd696c6734f4affacf
Oleh Strokan <oleg14ua71@gmail.com>
2024-06-08 13:04:28 +0200

    add get-server mock function

---
+++ load-balancer/weighted-round-robin/tests/mocks/get-server.go
@@ -0,0 +1,21 @@
+package mocks
+
+import . "load-balancer/app"
+
+type SimpleServerOverrides struct {
+	Address *string
+	Weight  *int
+}
+
+func GetRandomServer(overrides *SimpleServerOverrides) *SimpleServer {
+	testAddress := "http://localhost:8080"
+	testWeight := 10
+	if overrides.Address != nil {
+		testAddress = *overrides.Address
+	}
+	if overrides.Weight != nil {
+		testWeight = *overrides.Weight
+	}
+	server := CreateServer(testAddress, testWeight)
+	return server
+}
