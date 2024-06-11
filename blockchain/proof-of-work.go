9bafcc7b8e9c3c9f2a925866d709db6069f9eff7
Oleh Strokan <oleg14ua71@gmail.com>
2024-06-11 17:01:42 +0200

    blockchain: add proof-of-work creating

---
+++ blockchain/proof-of-work.go
@@ -0,0 +1,19 @@
+package blockchain
+
+import "math/big"
+
+const targetBits = 24
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
