67c4bf04da338e3c9aa486ceb741e2955e6ed2b4
Oleh Strokan <oleg14ua71@gmail.com>
2024-06-13 11:18:34 +0200

    blockchain: add iterator and add block methods to blockchain file

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -13,13 +13,10 @@ type Blockchain struct {
 	db  *bolt.DB
 }
 
-// TODO - replace blocks with db
-/*
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
-}*/
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
@@ -58,4 +55,63 @@ func CreateBlockchain() *Blockchain {
 	bc := Blockchain{tip, db}
 	return &bc
 
+}
+
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
+}
+
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializedBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	var lastHash []byte
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+		err = b.Put([]byte("l"), newBlock.Hash)
+
+		if err != nil {
+			log.Panic(err)
+		}
+
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
 }
