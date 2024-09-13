a29a730930c261b3326f2f160d8070c9faa5b7c8
Oleh Strokan <oleg14ua71@gmail.com>
2024-09-13 11:52:50 +0200

    load-balancer: lest response time

--- load-balancer/least-response-time/main.go
+++ load-balancer/least-response-time/main.go
@@ -1,1 +1,58 @@
-package least_response_time
+package main
+
+import (
+	"errors"
+	"net/url"
+	"sync"
+	"time"
+)
+
+type Server struct {
+	URL             *url.URL
+	AvgResponseTime time.Duration
+}
+
+type LoadBalancer struct {
+	servers []*Server
+	mu      sync.Mutex
+}
+
+func CreateLoadBalancer() *LoadBalancer {
+	return &LoadBalancer{
+		servers: make([]*Server, 0),
+	}
+}
+
+func (lb *LoadBalancer) AddServer(serverURL *url.URL) {
+	lb.mu.Lock()
+	defer lb.mu.Unlock()
+
+	lb.servers = append(lb.servers, &Server{
+		URL:             serverURL,
+		AvgResponseTime: 0,
+	})
+}
+
+func (lb *LoadBalancer) RemoveServer(serverURL *url.URL) error {
+	lb.mu.Lock()
+	defer lb.mu.Unlock()
+
+	for i, server := range lb.servers {
+		if server.URL.String() == serverURL.String() {
+			lb.servers = append(lb.servers[:i], lb.servers[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("server not found")
+}
+
+func (lb *LoadBalancer) GetServer() (*Server, error) {
+	lb.mu.Lock()
+	defer lb.mu.Unlock()
+
+	if len(lb.servers) == 0 {
+		return nil, errors.New("no servers available")
+	}
+
+	midInx := 0
+}
