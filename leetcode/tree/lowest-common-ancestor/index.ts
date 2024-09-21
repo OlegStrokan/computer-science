3a91fea6d8809a8cde56a35014b5e285f2431931
Oleh Strokan <oleg14ua71@gmail.com>
2024-09-21 10:28:23 +0200

    leetcode: lowest common ancestor

---
+++ leetcode/tree/lowest-common-ancestor/index.ts
@@ -0,0 +1,26 @@
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export const lowestCommonAncestor = (root: TreeNode | null, p: TreeNode, q: TreeNode):  TreeNode | null {
+    if (!root) {
+        return null;
+    }
+
+    if (p.val > root.val && q.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+
+    if (p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+
+    return root;
+}
