33eea2c362342c300ccb44972de4a8c5f2091896
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-02 22:47:01 +0200

    leetcode: longest subsquence

---
+++ leetcode/array/longest-subsequence/index.ts
@@ -0,0 +1,13 @@
+export const lengthOfLIS = (nums: number[]): number => {
+  const dp = new Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
