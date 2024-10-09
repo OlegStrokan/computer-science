4cdac01d16511bf92d861855064ccad6788bf565
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-09 21:46:00 +0200

    leetcode: update contains duplicate ii

--- leetcode/array/contains-duplicate-ii/index.ts
+++ leetcode/array/contains-duplicate-ii/index.ts
@@ -1,16 +1,16 @@
 export const containsNearbyDuplicate = (nums: number[], k: number): boolean => {
-  const window = new Set();
-  let left = 0;
+  const window = new Set<number>();
 
   for (let i = 0; i < nums.length; i++) {
-    if (i - left > k) {
-      window.delete(nums[left]);
-      left++;
-    }
     if (window.has(nums[i])) {
       return true;
     }
+
     window.add(nums[i]);
+
+    if (window.size > k) {
+      window.delete(nums[i - k]);
+    }
   }
   return false;
 };
