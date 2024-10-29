e727c1325342ee08b44e2fc220b0807475def848
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-29 09:36:02 +0100

    algorithms: factorial

---
+++ algorithms/factorial/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if (n <= 1) 
+    return 1;
+    else 
+    return n * factorial(n - 1);
+
+int main() {
+    int number;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers. \n");
+    } else {
+        printf("Factorial of %d is %d\n", number, factorial(number));
+    }
+
+    return 0;
+}
