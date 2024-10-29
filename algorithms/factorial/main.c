f36757b7cc0c91854ff856618e15f39781bdb9d2
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-29 11:25:15 +0100

    algorithms: factorial (tail-recursion)

--- algorithms/factorial/main.c
+++ algorithms/factorial/main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
-int factorial(int n) {
+int factorialRecursion(int n) {
   if (n <= 1) 
     return 1;
     else 
-    return n * factorial(n - 1);
+    return n * factorialRecursion(n - 1);
+}
+
+int factorialTailRecursion(int n, int accumulator) {
+    if (n <= 1) 
+        return accumulator;
+    else 
+        return factorialTailRecursion(n - 1, n* accumulator);
+}
+
+
+int factorialLoop(int n) {
+    int acc = 1;
+    for (int i = 1; i <= n; i++)
+        acc *= i;
+    return acc;
+}
 
 int main() {
     int number;
@@ -15,7 +31,9 @@ int main() {
     if (number < 0) {
         printf("Factorial is not defined for negative numbers. \n");
     } else {
-        printf("Factorial of %d is %d\n", number, factorial(number));
+        printf("Factorial of %d is %d\n", number, factorialRecursion(number));
+        printf("Factorial of %d is %d\n", number, factorialTailRecursion(number, 1));
+        printf("Factorial of %d is %d\n", number, factorialLoop(number));
     }
 
     return 0;
