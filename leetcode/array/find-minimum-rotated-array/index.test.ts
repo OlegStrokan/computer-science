c88e364c506bc21e7052e779f75e1375cf4304c8
Oleh Strokan <oleg14ua71@gmail.com>
2024-11-16 18:25:23 +0100

    leetcode: find minimum in rotated sorted array

---
+++ leetcode/array/find-minimum-rotated-array/index.test.ts
@@ -0,0 +1,16 @@
+export const findMinimumInRotatedSortedArray = (nums: number[]): number => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+};
